5e4b3cc1cd7ad96d284b22260bb4f24283a207ff
Md-Gulzeesh <gulzeesh2000@gmail.com>
2022-08-26 14:57:44 +0530

    Boneless MyRedwardbanner and Explore section added

---
+++ licios-clone/src/Components/BonelessCutSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProductCarousel from './ProductCarousel'
+
+const BonelessCutSection = () => {
+  return (
+    <div className="bonelessCutSection">
+      <h2 className='bonelessCutSectionHeading'>Boneless Cuts</h2>
+      <ProductCarousel />
+    </div>
+  );
+}
+
+export default BonelessCutSection
