4731e1a443444de3323609b402a447bd2a858048
Md-Gulzeesh <gulzeesh2000@gmail.com>
2022-08-27 17:51:41 +0530

    login and other modification

--- licios-clone/src/Components/NavbarSection.jsx
+++ licios-clone/src/Components/NavbarSection.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-// import { FaChevronDown } from "react-icons/fa";
-// import styles from "./NavbarSection.module.css";
 // Bright Gray :#eaeaea
 // Red pink shade:#d11243
 // Peach Orange:#ffdc93
@@ -15,6 +13,8 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 import Badge from "react-bootstrap/Badge";
 import { useContext } from "react";
 import { AuthContext } from "../Context.jsx/AuthContext";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const NavbarSection = () => {
   const auth = useContext(AuthContext);
@@ -26,8 +26,113 @@ const NavbarSection = () => {
     window.scrollY > 5 ? setFix(true) : setFix(false);
   };
   window.addEventListener("scroll", setFixed);
+
+  const [loginData, setLoginData] = useState("");
+  const [Otp, setOtp] = useState("");
+  const [OtpBtn, setOtpBtn] = useState(false);
+  const handleChange = (e) => {
+    if (Number(e.target.value[0]) === 0 && Number(e.target.value[1]) === 0) {
+      errorToast("Invalid Mobile Number");
+    }
+    setLoginData(e.target.value.trim());
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (loginData.trim().length === 0) {
+      errorToast("Please Enter Mobile Number");
+      return;
+    } else if (loginData.length > 10 || loginData.length < 10) {
+      errorToast("Invalid Mobile Number");
+      return;
+    } else {
+      setLoginData("");
+      setOtpBtn(true);
+      infoToast("OTP is 1234");
+      console.log(loginData);
+    }
+  };
+  const handleOtp = (e) => {
+    setOtp(e.target.value.trim());
+  };
+  const handleOtpSubmit = (e) => {
+    e.preventDefault();
+    if (Otp.trim() === "1234") {
+      successToast("Login Success");
+      handleClose();
+    } else {
+      errorToast("Invalid OTP");
+    }
+  };
+  const errorToast = (msg) =>
+    toast.error(msg, {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  const successToast = (msg) => {
+    toast.success(msg, {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  const infoToast = (msg) => {
+    toast.info(msg, {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
   return (
     <div>
+      {/* Toast */}
+      <ToastContainer
+        theme="colored"
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <ToastContainer
+        theme="colored"
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       {/* Navbar Top Section */}
       <div
         // className="navbarTop"
@@ -201,17 +306,29 @@ const NavbarSection = () => {
                   <Offcanvas.Body>
                     <h3 className="signInSignUpHeading">Sign In/Sign Up</h3>
                     <div className="loginForm">
-                      <Form>
-                        <Form.Control
-                          type="text"
-                          placeholder="Enter Mobile Number"
-                        />
-                        <Form.Control type="text" placeholder="Enter OTP" />
-
+                      <Form onSubmit={OtpBtn ? handleOtpSubmit : handleSubmit}>
+                        {OtpBtn ? (
+                          <Form.Control
+                            type="number"
+                            value={Otp}
+                            onChange={handleOtp}
+                            required
+                            placeholder="Enter OTP"
+                          />
+                        ) : (
+                          <Form.Control
+                            type="number"
+                            value={loginData}
+                            onChange={handleChange}
+                            required
+                            placeholder="Enter Mobile Number"
+                          />
+                        )}
                         <input
+                          onClick={OtpBtn ? handleOtpSubmit : handleSubmit}
                           className="proceedBtn mt-2"
                           type="submit"
-                          value="Proceed Via OTP"
+                          value={OtpBtn ? "Enter Otp" : "Proceed Via Otp"}
                         />
                       </Form>
                     </div>
